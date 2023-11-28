7e86dfb7f5e1f16b4d084760f0d64df3ec776ed2
matthewhiggins415 <mhiggins.louis@gmail.com>
2023-11-28 17:08:59 -0500

    added origins to cors.

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: ['https://pixmarketplace.com', 'http://localhost:8080'] }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
