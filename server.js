4997ad4559fe9de8d6c89712ceabb21721007b5d
matthewhiggins415 <mhiggins.louis@gmail.com>
2023-11-28 17:19:33 -0500

    added another cors thing.

--- server.js
+++ server.js
@@ -38,8 +38,11 @@ const app = express();
 
 app.use(cors({ origin: ['https://pixmarketplace.com', 'http://localhost:8080'] }));
 
+// Enable CORS for all routes
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://pixmarketplace.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
