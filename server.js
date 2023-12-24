bb1213949279aec9c829d55230fbf7c4eeeb3b86
matthewhiggins415 <mhiggins.louis@gmail.com>
2023-12-24 18:30:59 -0500

    added key

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const faqRoutes = require('./routes/faqRoutes.js');
 // connect to db
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`mongodb+srv://mhigginslouis:AUrVQMMSGXeScKqt@snapline.20yggtd.mongodb.net/`, {
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}`, {
       useNewUrlParser: true
     });
     
